a42ebf9a5c2562f3e9d1b7968f443a7438407193
susheetha <suhithamaheswaren598@gmail.com>
2026-06-04 15:40:12 +0530

    javascript variables,datatypes and variables completed

---
+++ javascript/script.js
@@ -0,0 +1,104 @@
+// var a = 10; //reinitialize & redeclare
+// let b = 20; //reinitialize
+// const c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num = 10;
+// var str ="susheetha";
+// var undefine;
+// var Null = null;
+// var boolean = true;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof undefine);
+// console.log(typeof Null);
+
+
+
+// var bigint = 123n;
+// var symbol = Symbol('li');
+
+// console.log(typeof bigint)
+// console.log(typeof symbol)
+
+
+// var arr = [10,20,30,40];
+// console.log(typeof arr , arr);
+// var obj = {
+//     name:"susheetha",
+//     dept:["AI&DS"]
+// }
+// console.log(typeof obj,obj)
+
+// //Arithmetic opertor (+,-,*,%,/)
+// var a =80;
+// var b = "suhi";
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(a/b);
+
+// logical operator(&&,||,!)
+//                   &&      ||     !
+//true true         true     true    false
+//true false        false    true      true
+//false true        false    true
+//false false       false    false
+
+// var a = "true";
+// var b = "false";
+// console.log (a&&b);
+// console.log(a||b);
+// console.log(!a);
+
+
+//relational operator(<,>,<=,>=,==,!=)
+
+// a = 10;
+// b = "20";
+
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);
+// asignment operator (=,+=,-=,%=,**=,/=)
+
+// a = 10;
+// b = 20;
+
+// a +=b; //a=a+b;
+
+// console.log(a);
+// a-=b; //a=a-b;
+// console.log(a);
+// a*=b; //a=a*b;
+// console.log(a);
+// a%=b;  //a=a%b;
+// console.log(a);
+// a/=b;   //a=a/b;
+// console.log(a);
+
+// var a = 10;
+// console.log(a++);
+// console.log(++a);
+
+// var a = 20;
+// console.log(--a);
+// console.log(a--);
+
+// a = 1;//2 3 
+// b = 1;// 0 1 2 
+// c = 0;//1 2 
+//               1    0       1    3       1    1     1    3
+// var result = a++ + --b +  ++c - ++a + ++b + c++ + b++ + a;
+// console.log(result)
