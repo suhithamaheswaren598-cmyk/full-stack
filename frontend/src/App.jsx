0b7c050e5cdccad4ddf32b6380032d3e443ddf42
susheetha <suhithamaheswaren598@gmail.com>
2026-06-07 15:40:00 +0530

    useEffect and react router completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,32 @@
 import Counter from "./components/Counter"
+import Effect from "./components/Effect"
 import Navbar from "./components/Navbar"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Home from "./pages/Home"
 import Parent from "./pages/Parent"
 import Services from "./pages/Services"
-
-const App = ()=>{
+import { Routes, Route } from 'react-router-dom'
+const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1>hello world!!</h1>
-      <h1>hello world!!</h1>
-      <Home />
-      <Contact />
-      <Services />
-      <About />
-      <Parent />
-      <Counter />
-    </div>
-    
-  )
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/parent' element={<Parent/>}/>
+        <Route path='/useState' element={<Counter/>}/>
+        <Route path='/useEffect' element={<Effect/>}/>
+      </Routes>
+      </>
+)
 }
+
+      
+    
+  
+
 
 export default App
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div>
             <nav>
                 <ul className="container">
-                    <li className="list"><a herf="">Home</a></li>
-                    <li className="list"><a href="">About</a></li>
-                    <li className="list"><a href="">Contact</a></li>
-                    <li className="list"><a href="">Services</a></li>
+                    <li className="list"><Link className='link'to="/">Home</Link></li>
+                    <li className="list"><Link className='link'to="/about">About</Link></li>
+                    <li className="list"><Link className ='link'to="/contact">Contact</Link></li>
+                    <li className="list"><Link className ='link'to="/services">Services</Link></li>
+                    <li className="list">
+                        <Link className="link" to='/parent'>props</Link>
+                    </li>
+                    <li className="list">
+                        <Link className="link" to='/useState'>useState</Link>
+                        </li>
+                        <li className="list">
+                        <Link className="link" to='/useEffect'>useEffect</Link>
 
+                    </li>
                 </ul>
             </nav>
         </div>
